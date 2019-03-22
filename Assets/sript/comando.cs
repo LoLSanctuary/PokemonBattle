ee581dd3f21b32502c6020f39b3b1da1903f48e7
unknown <33609@ETECAF.LOCAL>
2019-03-22 17:30:13 -0300

    aula 22

---
+++ Assets/sript/comando.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comando : MonoBehaviour {
+
+     int nivel = 5;
+     int poder = 9;
+     int atk = 14;
+     int modificador = 1;
+     public bool critico;
+     int tipoAtaque;
+
+
+	// Use this for initialization
+	void Start () {
+        int dano;
+
+        dano = ((((2 * nivel / 5) + 2 * poder * atk) / 50) + 2) * modificador;
+        if (critico)
+        {
+            dano = dano * 2;
+        }
+         Debug.Log(dano);
+	}
+}
---
+++ Assets/sript/int n.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intn : MonoBehaviour {
+
+    int numero ;
+  
+    
+    // Use this for initialization
+    void Start()
+    {
+      
+        imprimir(3);
+        imprimir(24);
+
+    }
+
+    void imprimir(int numero)
+    {
+
+        if (numero % 2 > 0)
+        {
+            Debug.Log("Estranho");
+        }
+        else if (numero >= 2 && numero <= 5)
+        {
+            Debug.Log("Não estranho");
+        }
+        else if (numero >= 6 && numero <= 20)
+        {
+            Debug.Log("Estranho");
+        }
+        else
+        {
+            Debug.Log("Não estranho");
+        }
+    }
+}
